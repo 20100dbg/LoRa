197159f1e9d07b061494445b598c98c4252e6723
20100dbg <vincent.marie80@gmail.com>
2024-08-13 16:56:20 +0200

    corrections deto

--- deto/deto_master.py
+++ deto/deto_master.py
@@ -1,56 +1,44 @@
 import sx126x
 import time
-import RPi.GPIO as GPIO
-from signal import signal, SIGINT
+import threading
 
-def cleanclose():
-    GPIO.cleanup()
-    exit(0)
 
-def handler(signal_received, frame):
-    cleanclose()
+def listener():
+    while isRunning:
+        data = lora.receive()
+        if data:
+            print("received : ", data)
+        time.sleep(0.1)
 
-def get_etat(pin):
-    return not GPIO.input(pin)
 
-def envoyer(adresse, msgid):
-    addr = adresse.to_bytes(1, 'big')
+def send(addr, msgid):
+    addr = addr.to_bytes(1, 'big')
     m = msg[msgid].to_bytes(1, 'big')
     lora.sendraw(addr + m)
 
 
-msg = {"PING": 0, "PONG": 1, "ARMER_ON": 2, "ACK_ARMER_ON": 3, "ARMER_OFF": 4, "ACK_ARMER_OFF": 5, "FEU": 6, "ACK_FEU" : 7 }
+
+msg = {"PING": 0, "PONG": 1, "ARMED_ON": 2, "ACK_ARMED_ON": 3, "ARMED_OFF": 4, "ACK_ARMED_OFF": 5, "FIRE": 6, "ACK_FIRE" : 7 }
 
 #MSG
 #ADRESSE (1) + MSG (1)
 
-signal(SIGINT, handler)
-
-pin_bouton = 21
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin_bouton, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-boutonActif = False
-debutBouton = time.time()
+buttonActive = False
+startButton = time.time()
 
 #initialize lora
 lora = sx126x.sx126x(channel=18,address=0,network=0)
 
-adresse_slave = 1
+
+isRunning = True
+t_receive = threading.Thread(target=listener)
+t_receive.start()
+
+
+print("msg : PING, ARMED_ON, ARMED_OFF, FIRE")
 
 while True:
+    addr = int(input("slave : "))
+    msgid = input("msg : ")
 
-    if not boutonActif and get_etat(pin_bouton):
-        debutBouton = time.time()
-        boutonActif = True
-
-    if boutonActif and not get_etat(pin_bouton):
-        boutonActif = False
-
-    if boutonActif and (time.time() - debutBouton) > 2:
-        envoyer(adresse_slave, msg["FEU"])
-        boutonActif = False
-
-
-    time.sleep(0.1)
+    send(addr, msgid)
--- deto/deto_slave.py
+++ deto/deto_slave.py
@@ -3,66 +3,72 @@ import time
 import RPi.GPIO as GPIO
 from signal import signal, SIGINT
 
-def cleanclose():
+def handler(signal_received, frame):
     GPIO.cleanup()
     exit(0)
-
-def handler(signal_received, frame):
-    cleanclose()
 
 def activer_relais():
     GPIO.output(pin_relais, GPIO.LOW)
     time.sleep(1)
     GPIO.output(pin_relais, GPIO.HIGH)
 
-def armer(armer):
-    return True
+def setArmed(state):
+    isArmed = state
 
+def send(addr, msgkey):
+    addr = addr.to_bytes(1, 'big')
+    msgkey = msgkey.to_bytes(1, 'big')
+    lora.sendraw(addr + msgkey)
 
-def envoyer(adresse, msgid):
-    addr = adresse.to_bytes(1, 'big')
-    m = msg[msgid].to_bytes(1, 'big')
-    lora.sendraw(addr + m)
-
+def getmsgkey(msgvalue):
+    idx = list(msg.values()).index(msgvalue)
+    return list(msg.keys())[idx]
 
 def listener():
+    isArmed = False
+
     while True:
         data = lora.receive()
 
         if data and len(data) == 2:
 
             addr = int.from_bytes(data[0:1])
-            msgid = int.from_bytes(data[1:2])
+            msgvalue = int.from_bytes(data[1:2])
 
-            if msg["PING"] == msgid:
-                envoyer(addresse_master, msg["PONG"])
-            elif msg["ARMER_ON"] == msgid:
-                armer(True)
-                envoyer(addresse_master, msg["ACK_ARMER_ON"])
-            elif msg["ARMER_OFF"] == msgid:
-                armer(False)
-                envoyer(addresse_master, msg["ACK_ARMER_OFF"])
-            elif msg["FEU"] == msgid:
-                activer_relais()
-                envoyer(addresse_master, msg["ACK_FEU"])
+            print("to :", addr, " - MSG :", getmsgkey(msgvalue))
+
+            if addr != addr_local:
+                continue
+
+            if msg["PING"] == msgvalue:
+                send(addr_master, msg["PONG"])
+            elif msg["ARMED_ON"] == msgvalue:
+                isArmed = True
+                send(addr_master, msg["ACK_ARMED_ON"])
+            elif msg["ARMED_OFF"] == msgvalue:
+                isArmed = False
+                send(addr_master, msg["ACK_ARMED_OFF"])
+            elif msg["FIRE"] == msgvalue:
+                if isArmed:
+                    activer_relais()
+                    send(addr_master, msg["ACK_FIRE"])
 
         time.sleep(0.1)
 
 
 signal(SIGINT, handler)
 
+global msg
+msg = {"PING": 0, "PONG": 1, "ARMED_ON": 2, "ACK_ARMED_ON": 3, "ARMED_OFF": 4, "ACK_ARMED_OFF": 5, "FIRE": 6, "ACK_FIRE" : 7 }
 
-msg = {"PING": 0, "PONG": 1, "ARMER_ON": 2, "ACK_ARMER_ON": 3, "ARMER_OFF": 4, "ACK_ARMER_OFF": 5, "FEU": 6, "ACK_FEU" : 7 }
-adresse = 1
-addresse_master = 0
-
-
+addr_local = 2
+addr_master = 0
 pin_relais = 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_relais, GPIO.OUT, initial=GPIO.HIGH)
 
 #initialize lora
-lora = sx126x.sx126x(channel=18,address=adresse,network=0)
+lora = sx126x.sx126x(channel=18,address=addr_local,network=0)
 
 listener()
